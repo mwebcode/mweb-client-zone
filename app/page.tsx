9b0230567b9e8fe8affd83330c0da5b96cf4995d
smvumvu-at-372412144220 <msikhanyisele@gmail.com>
2024-10-08 17:17:10 +0200

    Hiding the My Service Accounts table from the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-white">
           {/* Render the DemoPage which contains the table */}
-        <DemoPage />
+        {/*<DemoPage />*/}
         {/* <ServiceAccounts /> */}
         </main>
     
