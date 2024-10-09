90caab0163202c5615f299355b0e78f9f9b3386f
smvumvu-at-372412144220 <msikhanyisele@gmail.com>
2024-10-09 10:19:14 +0200

    Hiding the My Service Accounts table from the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <>
        <div className=''>
            <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-blue-100 to-white">
-               <h1 className="text-5xl font-serif text-gray-800">Welcome Mweb Client Zone!</h1>
+               <h1 className="text-5xl font-serif text-gray-800">Welcome To Mweb Client Zone!</h1>
                <p className="mt-4 text-lg text-gray-600 text-center max-w-md">
                    Site In-Progress
                </p>
